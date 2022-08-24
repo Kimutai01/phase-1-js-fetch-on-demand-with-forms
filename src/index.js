3ad2a63de50df66dc3314f4a89ae106d39ae2ff1
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-08-24 08:37:45 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
-}
+  const btn = document.querySelector("#sub");
+  const input = document.querySelector("#searchByID");
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(input.value);
+    fetch(`http://localhost:3000/movies/${input.value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const title = document.querySelector("section#movieDetails h4");
+        const summary = document.querySelector("section#movieDetails p");
 
-document.addEventListener('DOMContentLoaded', init);
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      })
+      .catch((err) => console.log(err));
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
